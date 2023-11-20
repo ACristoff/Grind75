9795d65ac78298a4ff80070d00f0a230865de6d7
Alex Cristoffanini <acristoffanini@gmail.com>
2023-11-20 15:14:47 -0800

    twosum exercise

---
+++ TwoSum.js
@@ -0,0 +1,19 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+const twoSum = (array, target) => {
+    const hashmap = {};
+    
+    for (let i = 0; i < array.length; i++) {
+        let complement = target - array[i]
+
+        if (hashmap.hasOwnProperty(complement)){
+          return [hashmap[complement], i]
+        }
+
+        hashmap[array[i]] = i;
+    }
+
+    return null;
+};
